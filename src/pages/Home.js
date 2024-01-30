bf5eb9a4772fabbe9a6daf06b3a60bd8c33651cc
silvalucas <lel.technologies.br@gmail.com>
2024-01-29 21:22:08 -0300

    fix: removes unused import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { useState } from 'react';
 import Topbar from '../components/topbar';
 import { Container } from '../components/container/style';
