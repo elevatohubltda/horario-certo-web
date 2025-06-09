72b99ae2f661a97e86b8c45b1d77ce1817e5f8f5
Lucas Oliveira <l_oliveiras@hotmail.com>
2025-06-09 20:09:05 -0300

    chore: changes api url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "http://horariocertoservice.elevatohub.com.br/",
+  baseURL: "https://horariocertoservice.elevatohub.com.br/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
