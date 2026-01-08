a93ff664af2bb18b9fb7e58f3f0f7531ca80efde
Lucas Oliveira <l_oliveiras@hotmail.com>
2026-01-07 21:14:34 -0300

    fix: adjusts some bugs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -145,7 +145,6 @@ export default function Home() {
     }
     try {
       const response = await createReservedSchedule(companyUrl, reservedSchedule);
-      console.log(response);
       setCode(response.data);
     } catch (error) {
       handleCompanySchedules();
